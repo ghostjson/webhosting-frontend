8a104cd0385f7f5120a0a9183a7292ecd16e3348
Akhil Ravindran <imakhilravindran@gmail.com>
2022-09-08 17:48:02 +0530

    intialized project

---
+++ assets/js/loading.js
@@ -0,0 +1,9 @@
+const openSpinner = () => {
+  const spinner = document.getElementById("spinner");
+  spinner.style.display = "flex";
+};
+
+const closeSpinner = () => {
+  const spinner = document.getElementById("spinner");
+  spinner.style.display = "none";
+};
